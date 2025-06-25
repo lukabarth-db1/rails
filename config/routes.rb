83971f243170967280ddd9fa619b6483eb480968
Luka Barth <luka.costa@db1.com.br>
2025-06-25 11:54:38 -0300

    create new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get "produtos/new", to: "produtos#new"
+  post "/produtos", to: "produtos#create"
+  get "/produtos/new", to: "produtos#new"
   root to: "produtos#index"
 end

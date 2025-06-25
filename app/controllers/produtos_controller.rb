2a175ec025e586fcf59d83bd418fd8492e0699c1
Luka Barth <luka.costa@db1.com.br>
2025-06-25 11:52:08 -0300

    controller for create new products

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -2,4 +2,22 @@ class ProdutosController < ApplicationController
     def index
         @produtos = Produto.order :preco
     end
+
+    def new
+        @produto = Produto.new
+    end
+
+    def create
+        @produto = Produto.new(produto_params)
+        if @produto.save
+            return redirect_to root_path
+        end
+
+        render :new, status: :unprocessable_entity
+    end
+
+    private
+    def produto_params
+        params.expect(produto: [:nome, :descricao, :preco, :quantidade])
+    end
 end
